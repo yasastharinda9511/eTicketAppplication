a18f7d8bad8ce482eeeeb1e7ccdc4250d8d8e417
Yasas <Yasas@LAPTOP-VG61JM8E>
2022-12-25 22:31:21 +0530

    Add project files.

---
+++ eTicketAppplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eTicketAppplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
